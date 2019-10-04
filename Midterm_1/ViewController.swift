b74b02af65310d7c1959d5a86e7cf042c2cd7850
Harrison Inocencio <hain222@uky.edu>
2019-10-04 15:32:58 -0400

    Add marker callouts

--- Midterm_1/ViewController.swift
+++ Midterm_1/ViewController.swift
@@ -32,21 +32,15 @@ class ViewController: UIViewController, MGLMapViewDelegate {
         // Set mapView delegate
         mapView.delegate = self
         
-        func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool
-        {
-        // Always allow callouts to popup when annotations are tapped.
-        return true
-        }
-        
-        func mapView(_ mapView: MGLMapView, didSelect annotation: MGLAnnotation)
-        {
-            let camera = MGLMapCamera(lookingAtCenter: annotation.coordinate, acrossDistance: 4500, pitch: 15, heading: 180)
-            mapView.fly(to: camera, withDuration: 4,
-            peakAltitude: 3000, completionHandler: nil)
-        }
+        // Allow the map view to show user's location
+        //mapView.showsUserLocation = true
         
     }
-
-
+        
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        // Always allow callouts to popup when annotations are tapped.
+        return true
+    }
+        
 }
 
