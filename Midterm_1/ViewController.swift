7eec37595631040244398192f97fb7d5dbc59009
Harrison Inocencio <hain222@uky.edu>
2019-10-04 22:57:42 -0400

    Add Route1 Selector button

--- Midterm_1/ViewController.swift
+++ Midterm_1/ViewController.swift
@@ -12,19 +12,11 @@ import Mapbox
 class ViewController: UIViewController, MGLMapViewDelegate {
     @IBOutlet var mapView: MGLMapView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        let map_center_coords = (38.030861, -84.504053)
-        
-        let r1_start_coords = (38.038366, -84.503865)
-        let r1_end_coords = (38.039977, -84.503839)
-        // Change mapbox style
-        mapView.styleURL = MGLStyle.satelliteStreetsStyleURL
-        
-        // Set map center and zoom
-        mapView.setCenter(CLLocationCoordinate2D(latitude: map_center_coords.0, longitude: map_center_coords.1), zoomLevel: 15, animated: false)
-        
+    let map_center_coords = (38.030861, -84.504053)
+    let r1_start_coords = (38.038366, -84.503865)
+    let r1_end_coords = (38.039977, -84.503839)
+    
+    @IBAction func R1Tapped(_ sender: Any) {
         // Add start point (map marker)
         let r1_start = MGLPointAnnotation()
         r1_start.coordinate = CLLocationCoordinate2D(latitude: r1_start_coords.0, longitude: r1_start_coords.1)
@@ -38,6 +30,20 @@ class ViewController: UIViewController, MGLMapViewDelegate {
         r1_end.title = "Gatton Student Center"
         r1_end.subtitle = "Ending Position"
         mapView.addAnnotation(r1_end)
+        
+        mapView.setCenter(CLLocationCoordinate2D(latitude: r1_start_coords.0, longitude: r1_start_coords.1), zoomLevel: 15, animated: true)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // Change mapbox style
+        mapView.styleURL = MGLStyle.satelliteStreetsStyleURL
+        
+        // Set map center and zoom
+        mapView.setCenter(CLLocationCoordinate2D(latitude: map_center_coords.0, longitude: map_center_coords.1), zoomLevel: 15, animated: false)
         
         // Set mapView delegate
         mapView.delegate = self
